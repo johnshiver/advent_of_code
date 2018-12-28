a9a8db3a3023d8aa6873bccb747fda8e7dfa3850
John Shiver <jshiver@squarespace.com>
2018-12-28 14:28:53 -0500

    day11 part2

--- 2018/day11/main.go
+++ 2018/day11/main.go
@@ -30,10 +30,10 @@ func calculatePower(x, y, serialNumber int) int {
 
 }
 
-func calcPowerLevelStartingAtPosition(x, y int, grid [][]int) int {
+func calcPowerLevelStartingAtPosition(x, y, degree int, grid [][]int) int {
 	power := 0
-	for i := x; i < len(grid[0]) && i < x+3; i++ {
-		for j := y; j < len(grid) && j < y+3; j++ {
+	for i := x; i < len(grid[0]) && i < x+degree; i++ {
+		for j := y; j < len(grid) && j < y+degree; j++ {
 			power += grid[j][i]
 		}
 	}
@@ -55,7 +55,7 @@ func part1() {
 	powerLevels := createGrid()
 	for y := 0; y < len(grid); y++ {
 		for x := 0; x < len(grid[0]); x++ {
-			powerLevels[y][x] = calcPowerLevelStartingAtPosition(x, y, grid)
+			powerLevels[y][x] = calcPowerLevelStartingAtPosition(x, y, 3, grid)
 		}
 	}
 
@@ -74,6 +74,65 @@ func part1() {
 	fmt.Println(finalX, finalY)
 }
 
+type Result struct {
+	x      int
+	y      int
+	power  int
+	degree int
+}
+
+type Work struct {
+	x      int
+	y      int
+	degree int
+}
+
+func part2() {
+	const WORKER_COUNT = 8
+	resultChan := make(chan *Result, 1000)
+	workChan := make(chan *Work, 10000)
+	serialNumber := 2866
+	grid := createGrid()
+	fillInPowerLevels(serialNumber, grid)
+
+	for i := 0; i < WORKER_COUNT; i++ {
+		go func() {
+			for work := range workChan {
+				power := calcPowerLevelStartingAtPosition(work.x, work.y, work.degree, grid)
+				resultChan <- &Result{x: work.x, y: work.y, power: power, degree: work.degree}
+			}
+		}()
+	}
+
+	go func() {
+		for y := 0; y < len(grid); y++ {
+			for x := 0; x < len(grid[0]); x++ {
+				for d := 0; d < 300; d++ {
+					workChan <- &Work{y: y, x: x, degree: d}
+				}
+			}
+		}
+		close(workChan)
+	}()
+
+	largestResult := &Result{}
+	resultsSeen := 0
+	for i := 0; i < 27000000; i++ {
+		r := <-resultChan
+		resultsSeen++
+		if r.power > largestResult.power {
+			largestResult = r
+		}
+		if resultsSeen%100000 == 0 {
+			fmt.Printf("Have seen %d results\n", resultsSeen)
+			fmt.Printf("Largest sofar: %v", largestResult)
+			fmt.Println()
+		}
+	}
+	fmt.Printf("\nLargest found: \n\tX: %d Y: %d Degree %d Power %d\n", largestResult.x+1, largestResult.y+1, largestResult.degree, largestResult.power)
+}
+
 func main() {
 	part1()
+	part2()
 }
