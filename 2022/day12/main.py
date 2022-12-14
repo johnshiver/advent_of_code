2713b5aaa61533c70051783dd4f0974a7a8536d1
John Shiver <john@planetx.com>
2022-12-14 10:41:23 -0500

    [2022/day12] part1 complete with dijkstras

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 from string import ascii_lowercase
+from collections import defaultdict
+import heapq as heap
 
 pos = namedtuple("pos", ["x", "y"])
 
@@ -47,79 +49,78 @@ def can_move(grid, x_pos: pos, y_pos: pos):
     return (ascii_lowercase.find(y) - ascii_lowercase.find(x)) <= 1
 
 
-def elevation_diff(grid, x_pos, y_pos):
-    y = grid[y_pos.y][y_pos.x]
-    x = grid[x_pos.y][x_pos.x]
-
-    # dont go to start
-    if y == "S":
-        return int(float("inf"))
-
-    # height diff for last move
-    if y == "E" and x == "z":
-        return 1
-    return ascii_lowercase.find(y) - ascii_lowercase.find(x)
-
-
 def distance(x, y):
     return abs(y.x - x.x) + abs(y.y - x.y)
 
 
-def find_shortest_path(grid, start: pos, end: pos, positions):
-    print(f"{start} {end} {grid[start.y][start.x]}")
+def create_graph(grid):
+    """
+    want dictionary of tuples (node, weight)
+    """
+    graph = {}
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            graph[pos(x, y)] = dict()
 
-    # if we have moved to end, we are done
-    if start == end:
-        return 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            curr = graph[pos(x, y)]
+            up = pos(x, y - 1)
+            down = pos(x, y + 1)
+            left = pos(x - 1, y)
+            right = pos(x + 1, y)
+            for adj in (up, down, left, right):
+                # add connections if we can move there
+                if can_move(grid, pos(x, y), adj):
+                    # for now, all weights are 1
+                    curr[adj] = 1
+    return graph
 
-    # can we go up
-    up = pos(start.x, start.y - 1)
-    down = pos(start.x, start.y + 1)
-    left = pos(start.x - 1, start.y)
-    right = pos(start.x + 1, start.y)
 
-    directions = [up, down, left, right]
-    best_choices = sorted(
-        [
-            (d, elevation_diff(grid, start, d), distance(d, end))
-            for d in directions
-            if can_move(grid, start, d)
-            and d not in positions
-            and elevation_diff(grid, start, d) <= 1
-        ],
-        key=lambda x: (x[1], x[2]),
-    )
+def dijkstra(graph, starting_node, end_node):
+    visited = set()
+    parents_map = {}
+    pq = []
+    node_costs = defaultdict(lambda: float("inf"))
+    node_costs[starting_node] = 0
+    heap.heappush(pq, (0, starting_node))
 
-    if not best_choices:
-        return float("inf")
+    while pq:
+        # go greedily by always extending the shorter cost nodes first
+        _, node = heap.heappop(pq)
+        visited.add(node)
 
-    best_paths = []
-    for direction, diff, _ in best_choices:
-        positions.add(direction)
-        d_weight = 1 + find_shortest_path(grid, direction, end, positions)
-        positions.remove(direction)
-        if start.x == 0 and start.y == 0:
-            print(f"{direction} {d_weight}")
-        best_paths.append(d_weight)
-        if diff == 1:
-            break
+        # after this is working, add in this short circuit
+        # we are done
+        # if node == end_node:
 
-    return min(best_paths)
+        for adj_node, weight in graph[node].items():
+            if adj_node in visited:
+                continue
+
+            new_cost = node_costs[node] + weight
+            if node_costs[adj_node] > new_cost:
+                parents_map[adj_node] = node
+                node_costs[adj_node] = new_cost
+                heap.heappush(pq, (new_cost, adj_node))
+
+    return parents_map, node_costs
 
 
 if __name__ == "__main__":
     print("# part 1------------------")
+
     t_grid = get_input("/Users/jshiver/projects/advent_of_code/2022/day12/test_input")
     print_grid(t_grid)
+    graph = create_graph(t_grid)
     start, end = get_starting_pos(t_grid)
-    positions = set()
-    # positions.add(start)
-    # assert find_shortest_path(t_grid, start, end, positions) == 31
-    print(find_shortest_path(t_grid, start, end, positions))
+    parents, costs = dijkstra(graph, start, end)
+    assert costs[end] == 31
 
-    # grid = get_input("/Users/jshiver/projects/advent_of_code/2022/day12/input")
-    # start, end = get_starting_pos(grid)
-    # positions = set()
-    # print(find_shortest_path(grid, start, end, positions))
+    t_grid = get_input("/Users/jshiver/projects/advent_of_code/2022/day12/input")
+    graph = create_graph(t_grid)
+    start, end = get_starting_pos(t_grid)
+    parents, costs = dijkstra(graph, start, end)
+    print(costs[end])
 
     print("# part 2------------------")
