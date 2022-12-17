0966aef63882550c494d8b5937301029f34d6bed
John Shiver <john@johnshiver.org>
2022-12-17 15:12:24 -0500

    [2022/day14] parts 1 + 2

--- 2022/day14/main.py
+++ 2022/day14/main.py
@@ -63,51 +63,139 @@ def draw_rocks_on_grid(grid, formations):
 
 
 def pour_sand(rock_grid, x, y):
-    """ """
+    """
+    A unit of sand always falls down one step if possible.
+
+    If the tile immediately below is blocked (by rock or sand)
+    the unit of sand attempts to instead move diagonally one step down and to the left
+
+    If that tile is blocked, the unit of sand attempts to instead move diagonally one step down and to the right
+    Sand keeps moving as long as it is able to do so
+    at each step trying to move down, then down-left, then down-right
+
+    If all three possible destinations are blocked
+    the unit of sand comes to rest and no longer moves
+    at which point the next unit of sand is created back at the source
+    """
     sand = pos(x, y)
-
     keep_going = True
-    while sand.y < len(rock_grid) and keep_going:
-        match rock_grid[sand.y][sand.x]:
+    moves = 0
+    while (
+        0 <= sand.y < len(rock_grid) and 0 <= sand.x < len(rock_grid[0]) and keep_going
+    ):
+        match rock_grid[sand.y + 1][sand.x]:
             case ".":
-                """ """
-            case "O":
-                """ """
-                # if there is a space to the left of O, take it
-                if rock_grid[sand.y][sand.x - 1] == ".":
-                    rock_grid[sand.y][sand.x - 1] = "O"
-                    keep_going = False
-                    break
+                sand = pos(sand.x, sand.y + 1)
+                moves += 1
+                continue
+            case "O" | "#":
+                # check diag left
+                match rock_grid[sand.y + 1][sand.x - 1]:
+                    # if that is blocked, try right
+                    case ".":
+                        sand = pos(sand.x - 1, sand.y + 1)
+                        moves += 1
+                        continue
+                    case "O" | "#":
+                        # check diag left
+                        match rock_grid[sand.y + 1][sand.x + 1]:
+                            case ".":
+                                sand = pos(sand.x + 1, sand.y + 1)
+                                moves += 1
+                                continue
+                            # if blocked
+                            case "O" | "#":
+                                rock_grid[sand.y][sand.x] = "O"
+                                keep_going = False
 
-                # if there is a space to the right of O, take it
-                if rock_grid[sand.y][sand.x + 1] == ".":
-                    rock_grid[sand.y][sand.x + 1] = "O"
-                    keep_going = False
-                    break
-            case "#":
-                rock_grid[sand.y - 1][sand.x] = "O"
-                keep_going = False
-                break
-        sand = pos(sand.x, sand.y + 1)
-    return True
+    return moves > 0
 
 
 if __name__ == "__main__":
     print("# part 1------------------")
-    rock_formations = get_input("/Users/jshiver/projects/advent_of_code/2022/day14/test_input")
-    x, y = get_grid_dimensions(rock_formations)
-    print(x, y)
-    grid = create_grid(x, y)
+    # rock_formations = get_input(
+    #     "/Users/johnshiver/projects/advent_of_code/2022/day14/test_input"
+    # )
+    # x, y = get_grid_dimensions(rock_formations)
+    # print(x, y)
+    # grid = create_grid(x, y)
+    # # print_grid([l[490:520] for l in grid])
+    # grid = draw_rocks_on_grid(grid, rock_formations)
+    # # for x in range(len(grid[-1])):
+    # #     grid[-1][x] = "#"
     # print_grid([l[490:520] for l in grid])
-    grid = draw_rocks_on_grid(grid, rock_formations)
-    print_grid([l[490:520] for l in grid])
 
-    for _ in range(5):
-        pour_sand(grid, 500, 0)
+    # count = 0
+    # keep_pouring = True
+    # while keep_pouring:
+    #     keep_pouring = pour_sand(grid, 500, 0)
+    #     count += 1
+    #     print(count)
+    #     print()
+    #     print_grid([l[490:520] for l in grid])
+    # # grid = draw_rocks_on_grid(grid, rock_formations)
+    # # grid = pour_sand(grid)
+    # # count sand on grid
+    # print(count)
+
+    # rock_formations = get_input(
+    #     "/Users/johnshiver/projects/advent_of_code/2022/day14/input"
+    # )
+    # x, y = get_grid_dimensions(rock_formations)
+    # grid = create_grid(x, y)
+    # grid = draw_rocks_on_grid(grid, rock_formations)
+
+    # count = 0
+    # keep_pouring = True
+    # while keep_pouring:
+    #     keep_pouring = pour_sand(grid, 500, 0)
+    #     count += 1
+    #     print(count)
+    # # grid = draw_rocks_on_grid(grid, rock_formations)
+    # # grid = pour_sand(grid)
+    # # count sand on grid
+    # print(count)
+
+    print("# part 2------------------")
+
+    rock_formations = get_input(
+        "/Users/johnshiver/projects/advent_of_code/2022/day14/test_input"
+    )
+    x, y = get_grid_dimensions(rock_formations)
+    grid = create_grid(x * 2, y + 2)
+    grid = draw_rocks_on_grid(grid, rock_formations)
+    for x in range(len(grid[-1])):
+        grid[-1][x] = "#"
+
+    count = 0
+    keep_pouring = True
+    while keep_pouring:
+        keep_pouring = pour_sand(grid, 500, 0)
+        count += 1
+        print(count)
         print()
-        print_grid([l[490:520] for l in grid])
+        print_grid([l[474:550] for l in grid])
     # grid = draw_rocks_on_grid(grid, rock_formations)
     # grid = pour_sand(grid)
     # count sand on grid
+    print(count)
 
-    print("# part 2------------------")
+    rock_formations = get_input(
+        "/Users/johnshiver/projects/advent_of_code/2022/day14/input"
+    )
+    x, y = get_grid_dimensions(rock_formations)
+    grid = create_grid(x * 2, y + 2)
+    grid = draw_rocks_on_grid(grid, rock_formations)
+    for x in range(len(grid[-1])):
+        grid[-1][x] = "#"
+
+    count = 0
+    keep_pouring = True
+    while keep_pouring:
+        keep_pouring = pour_sand(grid, 500, 0)
+        count += 1
+        print(count)
+    # grid = draw_rocks_on_grid(grid, rock_formations)
+    # grid = pour_sand(grid)
+    # count sand on grid
+    print(count)
